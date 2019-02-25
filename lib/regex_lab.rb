807630501f570065276eb8355af2b2e79133cde2
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-25 06:12:15 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-  word.scan(/\w+[aeiou]/)
+  if word.scan(/\w+[aeiou]/)
+    puts true
+  else
+    puts false 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
